b183e877bd00f88dd28d046fc84932ec5aef82e0
essitam <matisseam@hotmail.com>
2019-05-03 00:45:05 +0200

    changetext

--- jumping-o/char.js
+++ jumping-o/char.js
@@ -13,8 +13,8 @@ function setup() {
   textAlign(CENTER, CENTER);
 
         createP(
-          'I learn in this Letter, that Don Peter of Aragon, ' +
-            ' comes this night to Messina'
+        "The jumping o’s activated by the humidity-dust-meeting are the refusing of words or worlds appearing as solid units, uttering gasps and chuckles that are not-yet articulated blocks of sounds, colours, movements, sensations. An o quickly moves into a dot, just with the blink of an eye, with the falling of sand out of a stone with holes, with the stutters that will become the germs for new stories. The dots when just-not-completely-sure what is being said, written, heard, seen, felt, and even not sure who is talking, writing, hearing, seeing, feeling. An inarticulation that is full potential. For that stutter, in all its suspension, utters that which is superfluous, in need of a pause, invisible, too painful to tell, indefinable, or able to understood without the need of mediation. It slurps the event and sweeps it up into what is not yet there but already, or what was already but not anymore. What was felt in the slurping of the dots and to relay that feeling, to feel the felt, again, differently. Three dots of rhythmic suspension like three drops of liquid. Snot, rain, sweat, tears, spit, sips, drops, licks, coughs, puddles, drips, slurps, ink. Flows of liquids to attune to that which is left out, but still part of it. Like a leakage. Shifts, slippages, such slidings making gaps, rends, cuts. Not an absence though, perhaps an unwrapping."
+
         ).addClass('text').hide();
 
         texts = selectAll('.text');
